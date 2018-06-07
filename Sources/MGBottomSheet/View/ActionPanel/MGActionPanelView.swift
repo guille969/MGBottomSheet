6238a46d0473a59c99f8c08ef96d17a271c6cba2
Guille <guillermogarcia_18@hotmail.com>
2018-06-07 19:00:05 +0200

    Updated mg panel view

--- Sources/MGBottomSheet/View/ActionPanel/MGActionPanelView.swift
+++ Sources/MGBottomSheet/View/ActionPanel/MGActionPanelView.swift
@@ -8,24 +8,98 @@
 
 import UIKit
 
+protocol MGActionPanelViewDelegate: class {
+    func panelWillShow()
+    func panelWillHide()
+}
+
 class MGActionPanelView: UIView {
     
-    private var widthConstraint: NSLayoutConstraint!
+    public var widthConstraint: NSLayoutConstraint!
+    public var heightConstraint: NSLayoutConstraint!
+    
+    public var actionsPanelTitleView: MGPanelTitleView!
+    public var collectionView: MGBottomSheetCollectionView!
+    
+    private weak var delegate: MGActionPanelViewDelegate?
+    
+    init(withView parent: UIView, andDelegate delegate: MGActionPanelViewDelegate) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        self.delegate = delegate
+        self.configure(withParentView: parent)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
     //MARK: - Public Methods
     
-    public func configure(withParentView view: UIView) {
+    public func configureTitle(withTitle title: String?, actions: [ActionSheet], appearance: MGBottomSheetAppearanceAttributes, andDelegate delegate: MGBottomSheetCollectionViewDelegate) {
+        self.actionsPanelTitleView.configureTitle(title, withAppearance: appearance)
+        self.collectionView.collectionDelegate = delegate
+        self.collectionView.configure(withActions: actions, withAppearance: appearance)
+    }
+    
+    public func configureView(forNumberOfAction numberActions: Int, columns: Int, andView parent: UIView) {
+        self.resize(forNumberOfAction: numberActions, columns: columns, andView: parent)
+    }
+    
+    public func show() {
+        self.upDownAnimation(withDirection: 0)
+        self.isHidden = false
+        self.delegate?.panelWillShow()
+    }
+    
+    public func hide() {
+        self.upDownAnimation(withDirection: 1)
+        self.isHidden = true
+        self.delegate?.panelWillHide()
+    }
+    
+    //MARK: - Private Methods
+    
+    private func configure(withParentView view: UIView) {
+        self.backgroundColor = .white
         self.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(self)
         _ = self.leading(withView: view)
         _ = self.trailing(withView: view)
         _ = self.bottom(withView: view)
         _ = self.centerX(withView: view)
+        self.heightConstraint = self.height(constant: 165.0)
         self.widthConstraint = self.width(constant: 512.0)
         self.widthConstraint.isActive = false
         if self.traitCollection.horizontalSizeClass == .regular && self.traitCollection.verticalSizeClass == .regular {
             self.widthConstraint.isActive = true
         }
+        self.configureSubViews()
+    }
+    
+    private func configureSubViews() {
+        self.actionsPanelTitleView = MGPanelTitleView(withParentView: self, andDelegate: self)
+        self.collectionView = MGBottomSheetCollectionView(withView: self, andSibilig: self.actionsPanelTitleView)
+    }
+    
+    private func resize(forNumberOfAction numberActions: Int, columns: Int, andView parent: UIView) {
+        var actionsPanelHeight: CGFloat = 0.0
+        let maxActionsPanelHeight: CGFloat = (parent.frame.size.height * 0.7)
+        let topBottomSpace = self.collectionView.topConstraint.constant + 8
+        
+        if UI_USER_INTERFACE_IDIOM() == .pad {
+            let numberLines:Float = ceilf(Float(numberActions) / Float(columns))
+            actionsPanelHeight = self.actionsPanelTitleView.heightConstraint.constant + (CGFloat(kActionsViewHeigth) * CGFloat(numberLines)) + topBottomSpace
+        }
+        else {
+            actionsPanelHeight = self.actionsPanelTitleView.heightConstraint.constant + (CGFloat(kActionsViewHeigth) * CGFloat(numberActions)) + topBottomSpace
+        }
+        
+        if actionsPanelHeight > maxActionsPanelHeight {
+            actionsPanelHeight = maxActionsPanelHeight
+            self.collectionView.isScrollEnabled = true
+        }
+        
+        self.heightConstraint.constant = actionsPanelHeight
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -36,5 +110,13 @@ class MGActionPanelView: UIView {
             width.isActive = true
         }
         else { width.isActive = false }
+    }
+}
+
+//MARK: - MGPanelTitleViewDelegate
+
+extension MGActionPanelView: MGPanelTitleViewDelegate {
+    func titleDidHide() {
+        self.collectionView.topConstraint.constant = 8.0
     }
 }
